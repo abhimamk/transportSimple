b64172e29a1d3a67cdbe38e42793c7d18fe7e1b5
abhimamk <abhisek.m@pixelmindit.com>
2022-01-17 09:44:37 +0530

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-interface lines
-  {
-    straight: boolean;
-    startObj: object;
-    endObj: object;
-  }
+interface lines {
+  straight: boolean;
+  startObj: object;
+  endObj: object;
+}
 
 @Component({
   selector: 'app-root',
@@ -34,32 +33,47 @@ export class AppComponent implements OnInit {
 
   onAdd(): void {
     if (this.tpsForm.invalid) {
-      alert('hi');
+      console.log('invalid');
     }
     this.arr.push(this.tpsForm.value);
     if (this.arr.length === 1) {
-      this.tpsForm.reset();
+      // this.tpsForm.reset();
     }
     for (let i = 0; i < this.arr.length; i++) {
       for (let j = i + 1; j < this.arr.length; j++) {
-      if (this.arr[i].end === this.arr[j].start){
-        this.straightOrg = {straight: true, startObj: this.arr[i], endObj: this.arr[j]};
-        this.tpsForm.reset();
-        break;
-      } else if ( this.arr[i].start === this.arr[j].end) {
-        this.straightBLue = {straight: true, startObj: this.arr[i], endObj: this.arr[j]};
-        this.tpsForm.reset();
-        break;
-      } else if (this.arr[i].start === this.arr[j].start && this.arr[i].end === this.arr[j].end ){
-        this.tripLoop = {straight: true, startObj: this.arr[i], endObj: this.arr[j]};
-        this.tpsForm.reset();
-        break;
-      }
+        if (this.arr[i].end === this.arr[j].start) {
+          this.straightOrg = {
+            straight: true,
+            startObj: this.arr[i],
+            endObj: this.arr[j],
+          };
+          this.tpsForm.reset();
+          break;
+        } else if (this.arr[i].start === this.arr[j].end) {
+          this.straightBLue = {
+            straight: true,
+            startObj: this.arr[i],
+            endObj: this.arr[j],
+          };
+          this.tpsForm.reset();
+          break;
+        } else if (
+          this.arr[i].start === this.arr[j].start &&
+          this.arr[i].end === this.arr[j].end
+        ) {
+          this.tripLoop = {
+            straight: true,
+            startObj: this.arr[i],
+            endObj: this.arr[j],
+          };
+          this.tpsForm.reset();
+          break;
+        }
       }
     }
   }
 
-  onSlice(value): void{
-  return  value.slice(0, 3).toUpperCase( );
+  onSlice(value): void {
+    return value.slice(0, 3).toUpperCase();
   }
 }
